2e872d012f929f233460ea7b52bebbfbca0359b0
James Blackburn <blackburnfjames@gmail.com>
2022-10-19 16:58:57 +0100

    Added fully functioning hosts downloading

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+#[macro_use]
+extern crate fstrings;
+
 use std::collections::HashMap;
+use std::collections::HashSet;
+use std::fs;
 
 fn download_text<'a>(url: &str) -> Result<String, ureq::Error> {
     let body: String = ureq::get(url)
@@ -16,8 +21,8 @@ fn remove_comments(text: &str) -> String {
     return text.to_string();
 }
 
-fn hosts_to_hashmap(hosts: &str) -> HashMap<String, Vec<String>> {
-    let mut hash: HashMap<String, Vec<String>> = HashMap::new();
+fn hosts_to_hashmap(hosts: &str) -> HashMap<String, HashSet<String>> {
+    let mut hash: HashMap<String, HashSet<String>> = HashMap::new();
     for line in hosts.lines() {
         let cleared_line = remove_comments(line).trim().to_string();
         if (cleared_line.is_empty()) || (cleared_line.starts_with('#')) {
@@ -26,18 +31,19 @@ fn hosts_to_hashmap(hosts: &str) -> HashMap<String, Vec<String>> {
         let addresses: Vec<String> = cleared_line.split(' ').map(|s| s.to_string()).collect();
         let destination_host =addresses[0].as_str();
         if !hash.contains_key(destination_host) {
-            hash.insert(destination_host.to_string(), addresses[1..].to_vec() );
+            hash.insert(destination_host.to_string(),  HashSet::from_iter(addresses[1..].to_vec()));
         } else {
             hash.get_mut(destination_host).unwrap().extend(addresses[1..].to_vec());
         }
     }
-
     hash
 }
 
 fn main() -> Result<(), ureq::Error> {
     let mut urls: Vec<String> = Vec::new();
     urls.push(String::from("https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts"));
+
+    let compression_level = 9;
 
     let mut all_hosts = String::new();
 
@@ -49,6 +55,16 @@ fn main() -> Result<(), ureq::Error> {
 
     let hosts_hash = hosts_to_hashmap(all_hosts.as_str());
     
+    let mut hosts_text = String::new();
+
+    for (destination, addresses) in hosts_hash.iter() {
+        let addr_vec: Vec<String> = addresses.into_iter().map(|s| s.to_owned()).collect();
+        for chunk in addr_vec.chunks(compression_level) {
+            hosts_text.push_str(f!("{destination} {sources}\n", sources=chunk.join(" ")).as_str()) 
+        }
+    }
+
+    fs::write("hosts", hosts_text).expect("Expected Hosts file to be writable");
 
 
     println!("Hello, world!");
